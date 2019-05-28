5aef5b0ce98dfe500abfaa99c45e621cbc136e22
dtf8207 <dtf8207@PU8L891FNN2.vms.ad.varian.com>
2019-05-28 16:46:41 +0530

    first commit

---
+++ src/main/java/proj/maven/test/WebViewerTest/App.java
@@ -0,0 +1,13 @@
+package proj.maven.test.WebViewerTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/proj/maven/test/WebViewerTest/AppTest.java
@@ -0,0 +1,9 @@
+package proj.maven.test.WebViewerTest;
+
+class AppTest{
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+	
+}
